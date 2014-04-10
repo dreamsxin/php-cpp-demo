dffa4f9f20fd32a80c2ca20b14ee134d20a47947
Dreamsxin <dreamsxin@126.com>
2014-04-10 10:31:51 +0800

    增加类 MyClass

--- myextension/main.cpp
+++ myextension/main.cpp
@@ -1,17 +1,18 @@
+#include <string>
 #include <iostream>
 #include <phpcpp.h>
 
 /**
-* my_function_void()
-*/
+ * my_function_void()
+ */
 void my_function_void()
 {
     std::cout << "In my_function_void()" << std::endl;
 }
 
 /**
-* my_dump()
-*/
+ * my_dump()
+ */
 void my_dump(Php::Parameters &params)
 {
     std::cout << "In my_dump()" << std::endl;
@@ -21,6 +22,67 @@ void my_dump(Php::Parameters &params)
         std::cout << "Parameter " << i << ": " << params[i] << std::endl;
     }
 }
+
+/**
+ * MyCustomClass
+ */
+class MyCustomClass : public Php::Base // , public Php::Countable
+{
+private:
+    int _x = 3;
+    
+public:
+    MyCustomClass()
+    {
+        std::cout << "MyCustomClass::MyCustomClass()" << std::endl;
+    }
+
+    MyCustomClass(int value) : _x(value)
+    {
+        std::cout << "MyCustomClass::MyCustomClass(" << value << ")" << std::endl;
+    }
+
+    MyCustomClass(const MyCustomClass &that)
+    {
+        std::cout << "MyCustomClass::MyCustomClass copy constructor" << std::endl;
+    }
+    
+    virtual ~MyCustomClass()
+    {
+        std::cout << "MyCustomClass::~MyCustomClass" << std::endl;
+    }
+
+    virtual void __construct()
+    {
+        std::cout << "MyCustomClass::__construct" << std::endl;
+    }
+
+    virtual void __destruct()
+    {
+        std::cout << "MyCustomClass::__destruct" << std::endl;
+    }
+    
+    virtual Php::Value count() //override
+    {
+        return 33;
+    }
+
+	/**
+	 * $a = ['a' => 1, 'b' => 2];
+	 * $c = new MyClass;
+	 * $c->loopArray($a);
+	 */
+    void loop(Php::Parameters &params)
+    {
+        std::cout << "Array/Object contains " << params[0].size() << " items" << std::endl;
+        auto m = params[0].mapValue();
+
+        std::cout << "map contains " << m.size() << " items" << std::endl;
+        for(auto &i: m) {
+            std::cout << "key: " << i.first << " \t\tval: " << i.second << std::endl;
+        }
+    }
+};
 
 
 /**
@@ -47,8 +109,21 @@ extern "C" {
             Php::ByVal("a", Php::Type::Numeric),
             Php::ByVal("b", Php::Type::String)
         });
-        
-        // @todo    add your own functions, classes, namespaces to the extension
+
+        // we are going to define a class
+        Php::Class<MyCustomClass> customClass("MyClass");
+
+        customClass.property("property1", "prop1");
+        customClass.property("property2", "prop2", Php::Protected);
+
+        customClass.method("loopArray", &MyCustomClass::loop, {
+            Php::ByVal("arr", Php::Type::Array)
+        });
+        customClass.method("loopObject", &MyCustomClass::loop, {
+            Php::ByVal("obj", Php::Type::Object)
+        });
+
+		extension.add(customClass);
         
         // return the extension
         return extension;
